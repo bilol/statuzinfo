9d9d788e82731f1e0b952e3eafdf109dfdacff53
Your Name <your.email@example.com>
2024-06-27 12:36:24 +0800

    update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, redirect, url_for, flash
 import pandas as pd
 
@@ -53,3 +54,4 @@ def partnerships():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+    
